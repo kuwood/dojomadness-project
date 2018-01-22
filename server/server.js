740d3de37d97ee4a9fabae4feb83a297100cf97f
kuwood <keithunderwood510@gmail.com>
2018-01-22 14:24:38 -0800

    Checking the heroes endpoint

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get(`/api/heroes`, (req, res) => {
+  console.log('found heroes');
+})
+
 const server = app.listen(process.env.PORT || 8080, function () {
   console.log(`Listening on port ${server.address().port}`);
 });
