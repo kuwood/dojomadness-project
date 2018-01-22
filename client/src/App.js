c6e6b202187c95ea8398c5a43e5de105b4a54bc3
kuwood <keithunderwood510@gmail.com>
2018-01-22 15:20:06 -0800

    Create loading state

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Overwatch Heroes</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.loading && <p className='loading'>Loading...</p>}
       </div>
     );
   }
